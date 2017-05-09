8ceb9d3b94eefe8888f357ae82c0f051474f07ce
mstrperson <jason@dublinschool.org>
2017-05-08 20:13:58 -0400

    Added Player class and Money class

---
+++ TheWorld/Money.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld
+{
+    public class Money
+    {
+        public uint Platinum { get; set; }
+        public uint Gold { get; set; }
+        public uint Silver { get; set; }
+        public uint Copper { get; set; }
+
+        public override string ToString()
+        {
+            String output = "";
+            bool addSpace = false;
+
+            if (Platinum > 0)
+            {
+                output += String.Format("{0}P", Platinum);
+                addSpace = true;
+            }
+            if (Gold > 0)
+            {
+                output += String.Format("{0}{1}G", addSpace ? " " : "", Gold);
+                addSpace = true;
+            }
+            if (Silver > 0)
+            { 
+                output += String.Format("{0}{1}S", addSpace ? " " : "", Silver);
+                addSpace = true;
+            }
+            if (Copper > 0 || !addSpace)
+                output += String.Format("{0}{1}C", addSpace ? " " : "", Copper);
+
+            return output;
+        }
+    }
+}
---
+++ TheWorld/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld
+{
+    /// <summary>
+    /// A player in TheWorld
+    /// </summary>
+    public class Player
+    {
+        /// <summary>
+        /// The player's name
+        /// </summary>
+        public String Name { get; protected set; }
+
+        /// <summary>
+        /// What level is the player.
+        /// </summary>
+        public int Level { get; protected set; }
+
+        /// <summary>
+        /// Player's wallet
+        /// </summary>
+        public Money MoneyPouch { get; protected set; }
+
+        /// <summary>
+        /// The area that the player is currently in.
+        /// </summary>
+        public Area CurrentArea { get; protected set; }
+
+        /// <summary>
+        /// Create a new Player for the game.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="startArea"></param>
+        public Player(String name, ref Area startArea)
+        {
+            this.Name = name;
+            this.CurrentArea = startArea;
+            this.Level = 1;
+
+        }
+    }
+}
